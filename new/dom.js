ee9a94313d20ea7ccec9c9481e075a02ad3e7cd0
Pranay2799 <pranaychintu27@gmail.com>
2023-01-28 14:29:39 +0530

    extra

--- new/dom.js
+++ new/dom.js
@@ -1,8 +1,15 @@
-var li = document.getElementsByTagName('li');
-//items.style.fontWeight='bold'
-li[2].style.backgroundColor='green'
-
-for(var i=0; i<li.length;i++){
-    li[i].style.fontWeight="bold"
-  li[i].style.color="pink"
-}
+var itemlist=document.querySelector('#items');
+itemlist.parentNode.style.backgroundColor='#f4f4f4'
+itemlist.children[1].style.backgroundColor='yellow'
+itemlist.firstElementChild.textContent='hello 1'
+itemlist.lastElementChild.textContent='hello 4'
+itemlist.parentElementSibling.style.color='green';
+var newDiv = document.createElement('div');
+newDiv.className='hello';
+newDiv.id='hello1';
+newDiv.setAttribute('title','Hello Div');
+var newDivText=document.createTextNode('HEllo');
+newDiv.appendChild(newDivText);
+var container=document.querySelector('header.container');
+var h1=document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
